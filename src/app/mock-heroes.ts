78a736ba8d00d0fa3810a61899f4161a92500346
ArcanaOrpheus <drakooniano@gmail.com>
2019-05-08 21:15:58 +0200

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Keltuzhad' },
+	{ id:12, name: 'Ragnaros' },
+	{ id:13, name: 'Zagara' },
+	{ id:14, name: 'Dio' },
+	{ id:15, name: 'Jojo' },
+	{ id:16, name: 'Stalin' },
+	{ id:17, name: 'All might' },
+	{ id:18, name: 'Midoriya Shonen' },
+	{ id:19, name: 'Link' },
+	{ id:20, name: 'Mario Verde' }
+];
